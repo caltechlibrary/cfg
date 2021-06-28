e6e172091ef2a9bcefb70eaec89458469794447e
Robert Doiel <rsdoiel@gmail.com>
2021-06-28 13:47:56 -0700

    Added some tests using cltests

--- cfg/mysql.py
+++ cfg/mysql.py
@@ -1,32 +1,42 @@
 import os
 from configparser import ConfigParser
 
-def FindConfig(name = 'my.cnf'): 
-    # check the current directory first, then check the home directory
-    home = os.getenv('HOME')
+_home = os.getenv('HOME')
+
+def FindConfig(names = ['.my.cnf', 'my.cnf'], search_path = ['.', _home]): 
+    '''FindConfig checks each of the expected names in the search path
+       returning None if none are found, otherwise the first one found.
+       The defaults roughly mimic the MySQL search behavoir.
+    '''
     my_cnf = None
-    if os.path.exists(name):
-        my_cnf = name
-    elif os.path.exists(os.path.join(home, '.my.cnf')):
-        my_cnf = os.path.join(home,'.my.cnf')
+    for name in names:
+        for p in search_path:
+            if os.path.exists(os.path.join(p, name)):
+                my_cnf = os.path.join(p, name)
+                break
+        if my_cnf != None:
+            break
     return my_cnf
 
-def ParseConfig(name, database = None, host = 'localhost', port = 3306):
+def ParseConfig(fname, **kwargs):
+    '''ParseConfig takes a configuration filename and any
+       key/value pairs you with to use as defaults. It parses
+       the config file and overwrites the defaults'''
     client = None
     config = ConfigParser()
-    if config != None:
-        config.read(name)
-        # Set our handly defaults
-        client = { 
-            'host': host,
-            'port': int(port),
-            'database': database,
-            'user': None,
-            'password': None
-        } 
+    if (config != None) and (fname != None) :
+        config.read(fname)
+        # Set up our handly defaults
+        client = {}
+        for key in kwargs:
+            client[key] = kwargs[key]
         if 'client' in config:
             for key in config['client']:
-                client[key] = config['client'][key]
+                if key == 'port':
+                    client[key] = int(config['client'][key])
+                else:
+                    client[key] = config['client'][key]
         else:
             return None
     return client 
+
---
+++ cfg_test.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python3
+
+import os
+
+from cltests import TestSet, T, IsSuccessful
+from cfg.mysql import FindConfig, ParseConfig
+
+def getKeys(obj):
+    keys = []
+    for key in obj:
+        keys.append(key)
+    return keys
+    
+
+def testFindConfig():
+    # write out test conf file.
+    home = os.getenv("HOME")
+    os.mkdir
+    with open('testdata/my1.cnf', 'w') as f:
+        f.write('''
+[client]
+localhost = Lisbon
+database = Cyucos
+user = Maz
+password = Ocean
+''')
+    expect = 'testdata/my1.cnf'
+    got = FindConfig(names = ['my1.cnf'], search_path = ['testdata', '.', home])
+    t = T()
+    t.Expected(expect, got, "should find testdata location")
+    return t.Results()
+
+def testParse():
+    t = T()
+    my_cnf = FindConfig(names = ['my1.cnf'], search_path = ['testdata'])
+    conf = ParseConfig(my_cnf)
+    expect = {
+        "localhost": "Lisbon", "database": "Cyucos",
+        "user": "Maz", "password": "Ocean"
+        }
+    t.ExpectedList(getKeys(expect), getKeys(conf), "checking my1.cnf keys");
+    for key in conf:
+        t.Expected(expect[key], conf[key], "checking values my1.cnf")
+
+    conf = ParseConfig(my_cnf, Ada = "Lovelace", Blaise = "Pascal", Niklaus = "Wirth")
+    pairs = { "Ada": "Lovelace", "Blaise": "Pascal", "Niklaus": "Wirth"}
+    for key in pairs:
+        t.Expected(True, key in conf, "checking for {key} in {conf}")
+        if key in conf:
+            t.Expected(pairs[key], conf[key], "checking values for {key}")
+    return t.Results()
+
+
+if __name__ == '__main__':
+    ts = TestSet('cfg.mysql tests')
+    ts.add(testFindConfig)
+    ts.add(testParse)
+    IsSuccessful(ts.run())
